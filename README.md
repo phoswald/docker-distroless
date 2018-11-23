e713b7f55ff3f40bbfd8b3655917e2099254b60b
Philip Oswald <philip.oswald@gmail.com>
2018-11-23 01:11:01 +0100

    Add minimal C program

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # docker-distroless
+
 Experiments with minimalistic Docker images
+
+## Build and run image (C)
+
+    $ cd minimal-c
+    $ make run
+
+## Inspect base image and built image
+
+    $ docker save -o opensuse-leap-42.3.tar opensuse/leap:42.3
+    $ docker save -o minimal-c.tar minimal-c:latest
---
+++ minimal-c/minimal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+    puts("This is MINIMAL (in C)");
+
+    puts("");
+    printf("Arguments (count: %i):\n", argc);
+    for(int i = 0; i < argc; i++) {
+        printf("- '%s'\n", argv[i]);
+    }
+
+    puts("");
+    printf("Environment:\n");
+    char** current = environ;
+    while(*current) {
+        printf("- '%s'\n", *current);
+        current++;
+    }
+}
